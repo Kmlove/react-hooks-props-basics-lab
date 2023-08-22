819743d50435c1f85e3afc194f34da9f0ed421d1
Kim Love <kimberlymlove15@gmail.com>
2023-08-21 23:13:04 -0400

    All tests passing

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+
+function About(props) {
+  console.log(props)
+
+  function displayPTag(){
+    if(props.bio){
+      return <p>{props.bio}</p>
+    } else {
+      return null
+    }
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {displayPTag()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
